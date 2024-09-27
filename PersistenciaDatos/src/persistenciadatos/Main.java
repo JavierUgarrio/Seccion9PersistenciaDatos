504308d50a802177fd1734096bc71beadb4dbf45
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-27 09:53:05 +0200

    list, set y hash

--- PersistenciaDatos/src/persistenciadatos/Main.java
+++ PersistenciaDatos/src/persistenciadatos/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+      
         //List es una interface 
         List lista1 = new ArrayList();
         lista1.add(5);
@@ -14,7 +14,7 @@ public class Main {
         for(Object dato:lista1){
             System.out.println(dato);
         }
-        //funcion lamda
+//        //funcion lamda
         lista1.forEach(dato->{
             System.out.println(dato);
         });
@@ -28,6 +28,42 @@ public class Main {
         //elimina toda la lista el metodo clear
         lista1.clear();
         System.out.println(lista1);
+
+//SET te devuelve en un orden aleatorio, no se puede repetir el mismo dato dos veces
+
+        Set dias = new HashSet();
+        dias.add("lunes");
+        dias.add("martes");
+        dias.add("miercoles");
+        dias.add("jueves");
+        
+        dias.forEach(dia ->
+        System.out.println(dia));
+        
+//        //hay que indicar el nombre de elemento para eliminarlo
+        dias.remove("jueves");
+        System.out.println(dias);
+        dias.clear();
+        System.out.println(dias);
+
+//Map
+
+        Map numeros = new HashMap();
+        numeros.put(0, "uno");
+        numeros.put(1,15);
+        numeros.put(2, 4);
+        numeros.put(3,"cinco");
+        
+        System.out.println(numeros);
+        System.out.println(numeros.get(2));
+        
+        //Se puede iterar utilizando keySet que es el indice o values que son los valores
+        for(Object valores: numeros.values()){
+            System.out.println(valores);
+        }
+        
+        numeros.remove(1);
+        System.out.println(numeros);
     }
     
 }
