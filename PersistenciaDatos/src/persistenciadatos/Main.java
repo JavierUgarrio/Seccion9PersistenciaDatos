73636bb84e005f3b82e9cc1ff339d0f4c2a6b770
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-09-27 09:34:49 +0200

    List

--- PersistenciaDatos/src/persistenciadatos/Main.java
+++ PersistenciaDatos/src/persistenciadatos/Main.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package persistenciadatos;
+import java.util.*;
 
-/**
- *
- * @author User
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        //List es una interface 
+        List lista1 = new ArrayList();
+        lista1.add(5);
+        lista1.add("Joselito");
+        lista1.add(true);
+        System.out.println(lista1);
+        
+        for(Object dato:lista1){
+            System.out.println(dato);
+        }
+        //funcion lamda
+        lista1.forEach(dato->{
+            System.out.println(dato);
+        });
+        
+        System.out.println(lista1.get(2));
+        System.out.println(lista1.set(0, "Javier"));
+        System.out.println(lista1);
+        System.out.println(lista1.remove(0));
+        
+        System.out.println(lista1);
     }
     
 }
